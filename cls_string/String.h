6634bf0521351b92af89d813ef9cfeed559e3bc9
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-06-24 23:50:32 +0700

    Commit 2
    Add 3 Constructors
    Add destructor

--- cls_string/String.h
+++ cls_string/String.h
@@ -1,7 +1,23 @@
 #pragma once
+#include<cstring>
 class String
 {
 private:
 	char* str;
+public:
+	String(const char* data) {
+		int l = strlen(data) + 1;
+		this->str = new char[l];
+		for (int i = 0; i < l; i++)
+			this->str[i] = data[i];
+	}
+	String(int size) {
+		this->str = new char[size] {};
+	}
+	String() :String(80) {}
+	~String() {
+		delete[] this->str;
+	}
+	
 };
 
